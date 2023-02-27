bba0488e1016ce233c21e378e7ab5e8b1e75005b
NotVinz <90572371+NotVinz@users.noreply.github.com>
2023-02-27 04:31:53 +0100

    Update rotate.py
    Rotates all worksheets in a workbook. Compatible with merged cells.
    Original formatting is ignored at the moment.

--- rotate.py
+++ rotate.py
@@ -1,41 +1,69 @@
 # -*- coding: utf-8 -*-
-
-import xlrd
+import sys
+import xlrd3
 import xlwt
 
-def rotate(filename):
-	
-	data = xlrd.open_workbook(filename)  #读取原始xls或xlsx表格
-	
-	new_table = xlwt.Workbook(encoding='utf-8')  #定义输出excel文件
-	data_sheet = new_table.add_sheet('rotate')   #定义输出表格的工作表
-	table = data.sheets()[0]  #读取原始xls的第一张工作表
-	rowcount = table.nrows    #获取原始表格数据的行数和列数
-	colons = table.ncols
-	
-	rows = []
-	
-	
-	for m in range(0,rowcount):
-		cols = []
-		for i in range(0,colons):	
-			cols.append(table.cell(m,i).value)
-		rows.append(cols)	
-		#将原始表格数据存入列表（二维）
-	rotate_item = []
-	for item in rows:
-		item.reverse()
-		rotate_item.append(item)
-		#通过reverse函数将列数据倒序排列
-	num_rows = len(rotate_item[0])
-	num_cols = len(rotate_item)
-	for t in range(0,num_rows):
-		for s in range(0,num_cols):
-			data_sheet.write(t,s,rotate_item[s][t])
-			#重新生成excel表格
-	new_table.save("rotate.xls")
-	#保存xls文件
+# Finds out if a cell is part of a group of merged cells
+def isinmerged(row, column, merged_cells):
+    for square in merged_cells:
+        if square[0] <= row < square[1]:
+            if square[2] <= column < square[3]:
+                return True
+    return False
 
+def rotateworkbook(filename):
+    inp_Workbook = xlrd3.open_workbook(filename, formatting_info=True)
+    out_Workbook = xlwt.Workbook(encoding='utf-8')
+
+    for nsheet in list(range(len(inp_Workbook.sheets()))):
+        curr_inp_sheet = inp_Workbook.sheets()[nsheet]
+        curr_out_sheet = out_Workbook.add_sheet(
+            inp_Workbook.sheets()[nsheet].name)
+
+        #style = xlwt.easyxf('align: rotation 90')
+        style = xlwt.easyxf('align: rotation 90, vert centre, horiz centre')
+
+        inp_sheet_nrows = curr_inp_sheet.nrows
+        inp_sheet_ncols = curr_inp_sheet.ncols
+        merged_cells = curr_inp_sheet.merged_cells
+        orig_table = []
+        for m in range(0, inp_sheet_nrows):
+            cols = []
+            for i in range(0, inp_sheet_ncols):
+                cols.append(curr_inp_sheet.cell(m, i).value)
+            orig_table.append(cols)
+
+        rotate_table = []
+        for t in range(0, inp_sheet_nrows):
+            for s in range(0, inp_sheet_ncols):
+                if not isinmerged(t, s, merged_cells):
+                    curr_out_sheet.write(inp_sheet_ncols - s - 1, t, orig_table[t][s], style=style)
+                else:
+                    if False:
+                        print("Skipped: " + str(t) + "," + str(s))
+                        print("Val:")
+                        print(orig_table[t][s])
+                        print("")
+                    pass
+
+        for mgd in merged_cells:
+            if False:
+                print("Found group of merged cells:")
+                print([inp_sheet_ncols - mgd[3], inp_sheet_ncols - mgd[2] - 1, mgd[0], mgd[1] - 1])
+
+            curr_out_sheet.write_merge(inp_sheet_ncols - mgd[3], inp_sheet_ncols - mgd[2] - 1, mgd[0],
+                                       mgd[1] - 1,
+                                       orig_table[mgd[0]][mgd[2]],
+                                       style=style)
+
+    out_Workbook.save("rot_" + filename)
 
 if __name__ == "__main__":
-	rotate(filename)  #输入文件名
+    if len(sys.argv) != 2:
+        print("Something is wrong")
+        print("Please run as Rotate_Excel.py <Filename.xls>")
+        exit()
+    # rotate('caca.xls')  # input file name
+    filename = sys.argv[1]
+
+    rotateworkbook(filename)
